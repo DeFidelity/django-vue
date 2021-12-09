124d3c16913c0bda5c423a32ad49ac8f5cc30237
DeFidelity <adeyemihaki@gmail.com>
2021-12-09 01:27:05 +0100

    e fit get use

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1 class="has-text-centered title">WELCOME TO FIDvoice</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
